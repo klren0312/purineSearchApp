6eed8c0bfadf4a57d5d634d027783272086cdc71
klren <kuanglong0312@sina.com>
2020-01-24 13:09:56 +0800

    feat: 初始化项目

---
+++ components/cu-header/index.js
@@ -0,0 +1,52 @@
+// components/cu-header/index.js
+Component({
+  /**
+   * 组件的一些选项
+   */
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    bgColor: {
+      type: String,
+      default: ''
+    },
+    isCustom: {
+      type: [Boolean, String],
+      default: false
+    },
+    isBack: {
+      type: [Boolean, String],
+      default: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    Custom: app.globalData.Custom
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    backHandler () {
+      wx.navigateBack({
+        delta: 1
+      })
+    },
+    toHome () {
+      wx.relaunch({
+        url: '/paegs/index/index'
+      })
+    }
+  }
+})
